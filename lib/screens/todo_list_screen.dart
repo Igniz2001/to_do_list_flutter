963c58387b1ce89562cdd07ddbbbd4f7416b410e
Nagase <samael041203@gmail.com>
2024-03-20 20:32:16 -0500

    Update todo_list_screen.dart
    ListView implementado

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_flutter/models/task.dart';
+import 'package:to_do_list_flutter/widgets/task_tile.dart';
+import 'package:to_do_list_flutter/screens/add_task_screen.dart';
 
-class TodoListScreen extends StatelessWidget {
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<Task> tasks = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( //Scaffold como widget de la pantalla principal
-      appBar: AppBar( //AppBar con el Lista de tareas
+    return Scaffold(
+      appBar: AppBar(
         title: Text('Lista de Tareas'),
       ),
-      body: Center(
-        child: Text('Lista de tareas va a ir aquí'),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          final task = tasks[index];
+          return TaskTile(task: task);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddTaskScreen()),
+          ).then((newTaskTitle) {
+            if (newTaskTitle != null) {
+              setState(() {
+                tasks.add(Task(name: newTaskTitle));
+              });
+            }
+          });
+        },
+        tooltip: 'Agregar Tarea',
+        child: Icon(Icons.add),
       ),
     );
   }
